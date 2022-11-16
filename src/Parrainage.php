5b4e3283f8110bcd1e83102dc54080b902b8945c
cabrel sianou <sianou@gamail.com>
2022-11-16 05:43:40 +0100

    recuperation des tout les parrain et leur filleuls

--- src/Parrainage.php
+++ src/Parrainage.php
@@ -231,4 +231,14 @@ class Parrainage
         }
         $fun($this->getFilleulsByParrain(0, 9));
     }
+//getAllFilleulsWithParrain
+public function getAllFilleulsWithParrain()
+{ 
+ $query="SELECT p.FULLNAME ,p.PHONE ,p.EMAIL ,p.FACULTY, P.PICTURE , f.FULLNAME ,f.PHONE ,
+          f.EMAIL ,f.FACULTY ,f.PICTURE from parrain p JOIN filleuls f USING (IDPARRAIN)";
+          
+
+    return $this->con->query($query)->fetchAll();
+}
+
 }
